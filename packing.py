b2529afd777802145debba5c5300e1c128ef1d67
gsimoes00 <gustavomota001@gmail.com>
2020-12-11 17:49:38 -0300

    Added comments throughout the code

--- packing.py
+++ packing.py
@@ -1,15 +1,23 @@
 from enum import IntEnum
-from PIL import Image
 from copy import copy, deepcopy
 from random import random
 
-class TipoBloco(IntEnum):
-    VAZIO = 0
-    DISPONIVEL = 1
-    PREENCHIDO = 2
-    BURACO = 3
-    IMPOSSIVEL = 4
+try:
+    from PIL import Image
+    hasPIL = True
+except ImportError:
+    print("[AVISO] Voce nao possui o modulo PIL instalado, geracao e carregamento de imagens serao desabilitados!")
+    hasPIL = False
 
+#tipos de blocos
+class TipoBloco(IntEnum):
+    VAZIO = 0           #fora da area designada
+    DISPONIVEL = 1      #disponivel para ser preenchido
+    PREENCHIDO = 2      #ja preenchido
+    BURACO = 3          #buraco que nao foi possivel de ser preenchido
+    IMPOSSIVEL = 4      #buraco que não poderia ser preenchido de nenhuma maneira
+
+#enumeracao dos percursos de varredura e boustrophedon a serem testados
 enumPercursos = {
     0: (False, False, False, False),
     1: (False, False, False, True),
@@ -29,35 +37,49 @@ enumPercursos = {
     15: (True, True, True, True)
 }
 
+#enumeracao dos percursos espirais a serem testados
+#esperal descrescente foi removida por ser uma solucao ruim
 enumEspirais = {
-    0: (False, False, 0),
-    1: (False, False, 1),
-    2: (False, False, 2),
-    3: (False, False, 3),
-    4: (False, True, 0),
-    5: (False, True, 1),
-    6: (False, True, 2),
-    7: (False, True, 3),
-    8: (True, False, 0),
-    9: (True, False, 1),
-    10: (True, False, 2),
-    11: (True, False, 3),
-    12: (True, True, 0),
-    13: (True, True, 1),
-    14: (True, True, 2),
-    15: (True, True, 3)
+    0: (True, False, 0),
+    1: (True, False, 1),
+    2: (True, False, 2),
+    3: (True, False, 3),
+    4: (True, True, 0),
+    5: (True, True, 1),
+    6: (True, True, 2),
+    7: (True, True, 3),
+    #8: (False, False, 0),
+    #9: (False, False, 1),
+    #10: (False, False, 2),
+    #11: (False, False, 3),
+    #12: (False, True, 0),
+    #13: (False, True, 1),
+    #14: (False, True, 2),
+    #15: (False, True, 3)
 }
 
+#funcao utilitaria para a funcao random_fill()
+#assegura que a figura gerada se concentre no centro
+#e cresca aleatoriamente até próximo das bordas
+#modificar os pesos modifica o estilo de figura gerada
 def nova_chance(x, y, largura):
     if x == int(largura/2)  and y == int(largura/2):
         return 1
     else:
-        return (1 / (1 + ( ((x-(largura/2))/(largura/4))**2 + ((y-(largura/2))/(largura/4))**2) ))*0.30 + random()*0.70
+        return (1 / (1 + ( ((x-(largura/2))/(largura/4))**2 + 
+            ((y-(largura/2))/(largura/4))**2) ))*0.30 + random()*0.70
 
+#implementacao iterativa de um algoritmo recursivo baseado no 
+#flood fill porem com condicao de parada aleatoria
+#gera figuras para serem usadas no problema
+#as figuras se assemelham a "ilhas" porem com buracos
 def random_fill(matriz, largura, valor, chance, iniX, iniY):
     
     X = 0
     Y = 1
+    #a lista e usada como os proximos pontos a serem verificados
+    #substituindo a caracteristica recursiva
+    #manter a recursividade faria a pilha estourar em imagens largas
     lista = []
     lista.append((iniX, iniY))
     t = 1
@@ -79,6 +101,7 @@ def random_fill(matriz, largura, valor, chance, iniX, iniY):
         
         p += 1
 
+#implementacao do algoritmo bastante conhecido chamado flood fill
 def flood_fill(matriz, largura, preencher, valor, iniX, iniY):
     
     X = 0
@@ -103,76 +126,91 @@ def flood_fill(matriz, largura, preencher, valor, iniX, iniY):
         
         p += 1
 
+#preenche todos as celulas iguais a um certo valor
+#faz uma varredura completa da imagem e altera os valores
 def fill_all(matriz, largura, preencher, valor):
     for i in range(largura):
         for j in range(largura):
             if matriz[j][i] == preencher:
                 matriz[j][i] = valor
 
+#funcao que executa os passos para gerar a imagem aleatoria
 def matriz_aleatoria(largura):
 
+    #cria matriz vazia
     matriz = [[0 for _ in range(largura)] for __ in range(largura)]
 
+    #executa random_fill para gerar uma figura aleatoria
     random_fill(matriz, largura, 1, 1.0, largura//2, largura//2)
+    #executa flood fill no exterior da figura para mante-lo intacto
     flood_fill(matriz, largura, 0, 4, 0, 0)
+    #preenche os buracos da figura
     fill_all(matriz, largura, 0, 1)
+    #retorna o exterior para seu estado devido com outro flood fill
     flood_fill(matriz, largura, 4, 0, 0, 0)
 
     return matriz
 
+if hasPIL:
+    #recupera um problema para resolver de um arquivo de imagem
+    #qualquer pixel que não seja branco é considerado como fazendo parte da figura
+    #branco é considerado o exterior
+    def matriz_de_arquivo(nome):
+        img_src = Image.open(nome).convert(mode='RGB')
 
-def matriz_de_arquivo(nome):
-    img_src = Image.open(nome).convert(mode='RGB')
+        matrix = img_src.getdata()
 
-    matrix = img_src.getdata()
+        matriz = [[None for _ in range(img_src.width)] for __ in range(img_src.height)]
 
-    matriz = [[None for _ in range(img_src.width)] for __ in range(img_src.height)]
+        for i in range(0, len(matrix), img_src.width):
+            for j in range(img_src.width):
+                matriz[i//img_src.width][j] = TipoBloco.VAZIO if matrix[i+j] == (255, 255, 255) else TipoBloco.DISPONIVEL
 
-    for i in range(0, len(matrix), img_src.width):
-        for j in range(img_src.width):
-            matriz[i//img_src.width][j] = TipoBloco.VAZIO if matrix[i+j] == (255, 255, 255) else TipoBloco.DISPONIVEL
+        return matriz, img_src.width, img_src.height
 
-    return matriz, img_src.width, img_src.height
+    #cria uma imagem do problema para visualização
+    def matriz_para_imagem(matriz, largura, altura, fator, grade):
 
-def matriz_para_imagem(matriz, largura, altura, fator, grade):
+        if grade:
+            correcao = 1
+        else:
+            correcao = 0
+        fator = fator + correcao
 
-    if grade:
-        correcao = 1
-    else:
-        correcao = 0
-    fator = fator + correcao
+        lista_cores = [(255, 255, 255), (127, 127, 127), (16, 16, 192), 
+            (16, 192, 16), (192, 16, 192), (0, 255, 255), (127, 0, 0), 
+            (255, 64, 0), (255, 128, 0), (255, 192, 0), (255, 255, 0)]
+        paleta = [lista_cores[i][j] if i < len(lista_cores) else 0 
+            for i in range(256) for j in range(3)]
 
-    lista_cores = [(255, 255, 255), (127, 127, 127), (16, 16, 192), 
-        (16, 192, 16), (192, 16, 192), (0, 255, 255), (127, 0, 0), 
-        (255, 64, 0), (255, 128, 0), (255, 192, 0), (255, 255, 0)]
-    paleta = [lista_cores[i][j] if i < len(lista_cores) else 0 
-        for i in range(256) for j in range(3)]
+        img_dest = Image.new('P', (largura*fator + correcao, altura*fator + correcao), 
+            color=TipoBloco.VAZIO)
+        img_dest.putpalette(paleta)
+        pixels = img_dest.load()
 
-    img_dest = Image.new('P', (largura*fator + correcao, altura*fator + correcao), 
-        color=TipoBloco.VAZIO)
-    img_dest.putpalette(paleta)
-    pixels = img_dest.load()
-
-    if grade:
-        for i in range(altura*fator + correcao):
-            for j in range(largura*fator + correcao):
-                if i % fator == 0 or j % fator == 0:
-                    pixels[j,i] = 255
-                else:
+        if grade:
+            for i in range(altura*fator + correcao):
+                for j in range(largura*fator + correcao):
+                    if i % fator == 0 or j % fator == 0:
+                        pixels[j,i] = 255
+                    else:
+                        pixels[j,i] = matriz[i//fator][j//fator]
+        else:
+            for i in range(altura*fator):
+                for j in range(largura*fator):
                     pixels[j,i] = matriz[i//fator][j//fator]
-    else:
-        for i in range(altura*fator):
-            for j in range(largura*fator):
-                pixels[j,i] = matriz[i//fator][j//fator]
 
-    return img_dest
+        return img_dest
 
+#funcao utilitaria
+#checa se uma coordenada esta dentro ou fora dos limites da matriz
 def dentro(matriz, largura, altura, x, y):
     if x < 0 or y < 0 or not x < largura or not y < altura:
         return False
     else:
         return True
 
+#encontra a n-paridade de uma celula da matriz
 def paridade(matriz, largura, altura, par, x, y):
     lista = []
 
@@ -193,6 +231,7 @@ def paridade(matriz, largura, altura, par, x, y):
     
     return True
 
+#encontra quantas n-paridades uma celula possui
 def num_paridades(matriz, largura, altura, x, y):
     sum = 0
     for par in range(4):
@@ -200,6 +239,8 @@ def num_paridades(matriz, largura, altura, x, y):
             sum += 1
     return sum
 
+#faz o processo de embutir um bloco 2x2 no local especificado
+#so deve ser chamado apos checar que e possivel faze-lo
 def preencher(matriz, par, x, y):
     if par == 0:
         matriz[y][x] = TipoBloco.PREENCHIDO
@@ -222,15 +263,8 @@ def preencher(matriz, par, x, y):
         matriz[y-1][x] = TipoBloco.PREENCHIDO
         matriz[y-1][x-1] = TipoBloco.PREENCHIDO
 
-def preencher_paridade(matriz, largura, altura, par):
-    soma = 0
-    for y in range(altura):
-        for x in range(largura):
-            if paridade(matriz, largura, altura, par, x, y):
-                preencher(matriz, par, x, y)
-                soma += 1
-    #print(soma)
-
+#funcao de visualizacao, gera um mapa de tons de vermelho, laranja e amarelo
+#mostrando o numero de n-paridades de cada celula
 def mapear_paridade(matriz, largura, altura):
 
     PAR_OFFSET = 6
@@ -241,6 +275,8 @@ def mapear_paridade(matriz, largura, altura):
         for x in range(largura):
             matriz[y][x] = num_paridades(matriz_cp, largura, altura, x, y) + PAR_OFFSET
 
+#funcao de visualizacao
+#serve para colocar a cor verde nos buracos
 def pintar_buracos(matriz, largura, altura):
     for y in range(altura):
         for x in range(largura):
@@ -248,6 +284,8 @@ def pintar_buracos(matriz, largura, altura):
                 num_paridades(matriz, largura, altura, x, y) == 0):
                 matriz[y][x] = TipoBloco.BURACO
 
+#funcao que gera os percursos de varredura e boustrophedon
+#suas variaveis controlam as carcteristicas do percurso
 def gerarPercurso(bpd, startOnX, invX, invY, rangeX, rangeY):
     lista = []
     rangeX = list(reversed(rangeX)) if invX else rangeX
@@ -262,6 +300,8 @@ def gerarPercurso(bpd, startOnX, invX, invY, rangeX, rangeY):
             rangeB = list(reversed(rangeB))
     return lista
 
+#funcao utilitaria da funcao que gera percursos espirais
+#serve para produzir segmentos da espiral em uma dada ordem
 def parte_espiral(num, xMinMax, yMinMax):
 
     lista = []
@@ -283,6 +323,9 @@ def parte_espiral(num, xMinMax, yMinMax):
 
     return lista
 
+#funcao que gera os percursos espirais a serem testados
+#suas variaveis controlam as carcteristicas do percurso
+#esse algoritmo e uma implementacao iterativa de um algoritmo recursivo
 def percurso_espiral(xMinMax, yMinMax, invertido, sentido, ordem):
     
     lista = []
@@ -322,6 +365,8 @@ def percurso_espiral(xMinMax, yMinMax, invertido, sentido, ordem):
 
     return lista
 
+#essa funcao meramente chama as outras funcoes de gerar cada tipo de percurso
+#e os coloca juntos numa lista e os retorna
 def gerarPercursos(rangeX, rangeY):
     listas = [None for _ in range(
         len(enumPercursos) + len(enumEspirais))]
@@ -344,6 +389,10 @@ def gerarPercursos(rangeX, rangeY):
 
     return listas
 
+#essa funcao simula o preenchimento em um dos percursos
+#percorrendo as celulas da maneira especificada por ele e
+#calculando quandos blocos seriam inseridos para cada
+#n-paridade relevante. retorna esses valores ao final
 def calcular_percurso(matriz, largura, altura, percurso):
     
     soma = [0, 0, 0, 0]
@@ -357,9 +406,11 @@ def calcular_percurso(matriz, largura, altura, percurso):
         
         soma[p] = preencher_percurso(matrizes[p], largura, altura, percurso, p)
 
-    #print(soma) #DEBUG
+    print(soma) #DEBUG
     return soma
 
+#essa funcao usa calcular_percurso() para descobrir qual dos percursos
+#gerados e o melhor e deveria ser escolhido
 def melhor_percurso(matriz, percursos, largura, altura):
 
     melhor_soma = [0, 0, 0, 0]
@@ -376,13 +427,14 @@ def melhor_percurso(matriz, percursos, largura, altura):
                 melhor_percurso = percurso
 
     #DEBUG
-    #print((percursos.index(melhor_percurso), melhor_paridade, melhor_soma[melhor_paridade]))
+    print((percursos.index(melhor_percurso), melhor_paridade, melhor_soma[melhor_paridade]))
 
     return (melhor_percurso, 
         percursos.index(melhor_percurso), 
         melhor_paridade, 
         melhor_soma[melhor_paridade])
 
+#essa funcao e utilizada para preencher e para simular o preenchimento de percursos
 def preencher_percurso(matriz, largura, altura, percurso, par):
     soma = 0
     
@@ -393,6 +445,8 @@ def preencher_percurso(matriz, largura, altura, percurso, par):
     
     return soma
 
+#funcao de visualizacao
+#usada para pintar os blocos impossiveis de rosa
 def pintar_impossiveis(matriz, largura, altura):
     for y in range(altura):
         for x in range(largura):
@@ -401,6 +455,7 @@ def pintar_impossiveis(matriz, largura, altura):
 
                 matriz[y][x] = TipoBloco.IMPOSSIVEL
 
+#funcao para gerar metricas para relatorio em texto
 def borda(matriz, largura, altura, x, y):
 
     if (matriz[y][x] != TipoBloco.VAZIO and 
@@ -414,6 +469,8 @@ def borda(matriz, largura, altura, x, y):
     
     return False
 
+#funcao de visualizacao
+#utilizada para depuracao (debug)
 def pintar_borda(matriz, largura, altura):
     for y in range(altura):
         for x in range(largura):
@@ -421,6 +478,8 @@ def pintar_borda(matriz, largura, altura):
 
                 matriz[y][x] = 5
 
+#funcao utilizada para gerar relatorios sobre as quantidades de cada
+#bloco ou caracteristica relevante da solucao
 def quantidade_blocos(matriz, largura, altura):
     soma = [0 for _ in range(len(TipoBloco) + 4)]
 
@@ -441,6 +500,7 @@ def quantidade_blocos(matriz, largura, altura):
 
     return soma
 
+#funcao que formata e imprime o relatorio informativo
 def gerar_relatorio(quantidades, largura, altura, tipo, embutidos):
     
     print('Blocos 2x2 embutidos: ' + str(embutidos))
@@ -450,9 +510,9 @@ def gerar_relatorio(quantidades, largura, altura, tipo, embutidos):
     elif tipo < 16:
         print('Boustrophedon ' + str(tipo - 8))
     elif tipo < 24:
-        print('Espiral Decrescente ' + str(tipo - 16))
-    elif tipo < 32:
-        print('Espiral Crescente ' + str(tipo - 24))
+        print('Espiral Crescente ' + str(tipo - 16))
+    #elif tipo < 32:
+        #print('Espiral Decrescente ' + str(tipo - 24))
     print('Largura x Altura: ' + str(largura) + ' x ' + str(altura) + ' = ' + str(largura*altura))
     print('Celulas nao vazias: ' + str(quantidades[2]+quantidades[3]+quantidades[4]) + ' = {Preenchidas: ' + str(quantidades[2]) + 
         '; Buracos: ' + str(quantidades[3]) + '; Impossiveis: ' + str(quantidades[4]) + '}')
@@ -469,6 +529,7 @@ def gerar_relatorio(quantidades, largura, altura, tipo, embutidos):
     except ZeroDivisionError:
         pass
 
+#funcao que corta a imagem deixando apenas a porcao relevante para o problema
 def crop(matriz, largura, altura):
     minX = minY = 999999
     maxX = maxY = 0
@@ -497,20 +558,27 @@ def crop(matriz, largura, altura):
     
     return nova_matriz, nova_largura, nova_altura
 
+#funcao facilitadora que refaz o processo de gerar imagem aleatoria
+#garantindo os limites desejados e o recorte apropriado da matriz
+def matriz_aleatoria_limitada(minL, maxL):
+    largura = altura = maxL
+    matriz = matriz_aleatoria(largura)
+    matriz, largura, altura = crop(matriz, largura, altura)
+    while largura < minL or altura < minL:
+        largura = altura = maxL
+        matriz = matriz_aleatoria(largura)
+        matriz, largura, altura = crop(matriz, largura, altura)
+    
+    return matriz, largura, altura
 
 ######################################################################################
 
 def main():
 
-    #matriz, largura, altura = matriz_de_arquivo('../imagens/artigo.png')
+    #usar para obter problema de arquivo, apenas se tiver o modulo PIL
+    #matriz, largura, altura = matriz_de_arquivo('..nome_do_arquivo.png')
 
-    largura = altura = 64
-    matriz = matriz_aleatoria(largura)
-    matriz, largura, altura = crop(matriz, largura, altura)
-    while largura < 16 or altura < 16:
-        largura = altura = 64
-        matriz = matriz_aleatoria(largura)
-        matriz, largura, altura = crop(matriz, largura, altura)
+    matriz, largura, altura = matriz_aleatoria_limitada(16, 64)
 
     pintar_impossiveis(matriz, largura, altura)
     percursos = gerarPercursos(range(largura), range(altura))
@@ -520,7 +588,7 @@ def main():
 
     gerar_relatorio(quantidade_blocos(matriz, largura, altura), largura, altura, tipo, embutidos)
 
-    #matriz_para_imagem(matriz, largura, altura, 8, True).show()
+    matriz_para_imagem(matriz, largura, altura, 8, True).show()
 
 if __name__ == "__main__":
     main()
